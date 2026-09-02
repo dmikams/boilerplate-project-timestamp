3bd7d62b295e4be46706a3d7d1bb7554c6064f04
unknown <dannahsanchez0118@gmail.com>
2026-09-03 01:38:54 +0800

    FCC wcserver 1

--- index.js
+++ index.js
@@ -24,6 +24,28 @@ app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
+app.get("/api/:date?", function (req, res) {
+  let date;
+
+  if (!req.params.date) {
+    date = new Date();
+  } else if (!isNaN(req.params.date)) {
+    date = new Date(Number(req.params.date));
+  } else {
+    date = new Date(req.params.date);
+  }
+
+  if (date.toString() === "Invalid Date") {
+    return res.json({
+      error: "Invalid Date"
+    });
+  }
+
+  res.json({
+    unix: date.getTime(),
+    utc: date.toUTCString()
+  });
+});
 
 
 // Listen on port set in environment variable or default to 3000
